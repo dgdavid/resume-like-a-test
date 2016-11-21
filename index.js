387bfd5a43c32ad53442df35d2dc08f6a23999d8
David Díaz <diazglezdavid@gmail.com>
2016-11-21 23:34:27 +0000

    Move the configuration load to a module

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 import fs from 'fs'
 import yaml from 'yaml-js'
+import config from './lib/config'
 import Resume from './lib/Resume'
 
 console.log('\x1B[2J\x1B[0f') // clear terminal, https://gist.github.com/KenanSulayman/4990953
-
-console.log('\nLoading `config.yaml` ...')
-const configFile = fs.readFileSync('./config.yaml')
-const config = yaml.load(configFile)
 
 console.log('\nLoading `data.yaml` ...')
 const dataFile = fs.readFileSync('./resume.yaml')
 const data = yaml.load(dataFile)
 
 console.log('\nWritting the resume...')
-new Resume(data, config).writeTo(config.outputFile)
+new Resume(data).write()
 
 console.log(`\nResume was created at ${config.outputFile}.`)
