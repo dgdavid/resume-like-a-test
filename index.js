78db19128364f746da7cff4946cd0ea154414392
David Díaz <diazglezdavid@gmail.com>
2016-12-03 18:45:22 +0000

    Update index file

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-import fs from 'fs'
-import yaml from 'yaml-js'
 import config from './lib/config'
+import pdf from './lib/Document'
 import Resume from './lib/Resume'
 
 console.log('\x1B[2J\x1B[0f') // clear terminal, https://gist.github.com/KenanSulayman/4990953
 
-console.log('\nLoading `data.yaml` ...')
-const dataFile = fs.readFileSync('./resume.yaml')
-const data = yaml.load(dataFile)
-
-console.log('\nWritting the resume...')
+console.log('\nGenerating resume ...')
 Resume.generate()
 
-console.log(`\nResume was created at ${config.outputFile}.`)
+console.log('\nWriting document ...')
+pdf.end()
+
+console.log(`\nResume was created at ${config.outputFilename}`)
